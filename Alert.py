5103692f8a3e0f9067eaf3e0c6fc2c28e0101e3f
Berk Efe Keskin <92454444+AkioKiyota@users.noreply.github.com>
2022-05-25 17:53:45 +0300

    Update Alert.py

--- Alert.py
+++ Alert.py
@@ -1,32 +1,43 @@
 
+
+
 from datetime import datetime
 import smtplib
 import getpass
+import os
+
+path__ = os.path.abspath(os.getcwd())
+
+print(path__)
+
+with open(path__ + "\data.txt", "r") as db:
+    data = db.read().split(",")
+    emailAdress = data[0]
+    emailPassword = data[1]
+
+    recevier = data[2]
+
+    user = getpass.getuser()
+    now = datetime.now()
+
+    currentTime = now.strftime("%H:%M:%S")
 
 
-emailAdress = input("Sender email address:\n")
-emailPassword = input("Sender email password:\n")
-
-recevier = input("recevier email address:\n")
-
-user = getpass.getuser()
-now = datetime.now()
-
-currentTime = now.strftime("%H:%M:%S")
-
-
-with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-    smtp.ehlo()
-    smtp.starttls()
-    smtp.ehlo()
+    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.ehlo()
+        
+        smtp.login(emailAdress, emailPassword)
+        
+        subj= 'Logged In!'
+        body = f'You logged in as {user}, at {currentTime}'
+        
+        msg = f'Subject: {subj}\n\n{body}'
+        smtp.sendmail(emailAdress, recevier, msg)
     
-    smtp.login(emailAdress, emailPassword)
     
-    subj= 'Logged In!'
-    body = f'You logged in as {user}, at {currentTime}'
     
-    msg = f'Subject: {subj}\n\n{body}'
-    smtp.sendmail(emailAdress, recevier, msg)
     
     
     
